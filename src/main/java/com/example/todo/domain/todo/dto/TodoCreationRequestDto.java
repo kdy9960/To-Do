60c2f88b0c1951b9d556bc5547cea36eca57355d
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2025-02-18 14:20:41 +0900

    TodoCreationRequestDto, ResponseDto 구성

---
+++ src/main/java/com/example/todo/domain/todo/dto/TodoCreationRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.todo.domain.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoCreationRequestDto {
+
+    private String description;
+
+}
