791642c1dc32a019121d8c52f9312a252ffd74aa
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2025-02-18 16:38:44 +0900

    Feature/todo 할일 전체 조회 기능구현

--- src/main/java/com/example/todo/domain/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/domain/todo/controller/TodoController.java
@@ -2,10 +2,13 @@ package com.example.todo.domain.todo.controller;
 
 import com.example.todo.domain.todo.dto.TodoCreationRequestDto;
 import com.example.todo.domain.todo.dto.TodoCreationResponseDto;
+import com.example.todo.domain.todo.entity.Todo;
 import com.example.todo.domain.todo.service.TodoService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,10 @@ public class TodoController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(todoCreationResponseDto);
+    }
+
+    @GetMapping("/today")
+    public List<Todo> getTodos() {
+        return todoService.getAllTodos();
     }
 }
