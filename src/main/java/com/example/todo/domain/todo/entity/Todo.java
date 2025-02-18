c3b71cd361ce7b8abcae3c7249ee488a467bd318
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2025-02-18 16:05:06 +0900

    Entity 수정

--- src/main/java/com/example/todo/domain/todo/entity/Todo.java
+++ src/main/java/com/example/todo/domain/todo/entity/Todo.java
@@ -4,17 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Todo {
 
     @Id
@@ -26,4 +27,10 @@ public class Todo {
     private boolean completed;
 
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.completed = false; // 기본값 설정
+        this.createdAt = LocalDateTime.now(); // 생성 시 자동으로 시간 설정
+    }
 }
