bd8927bc91c85095ed45e8c885086d425e3afd8f
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2025-02-18 15:27:43 +0900

    Entity 수정

--- src/main/java/com/example/todo/domain/todo/entity/Todo.java
+++ src/main/java/com/example/todo/domain/todo/entity/Todo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,10 +21,9 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-
     private String description;
 
     private boolean completed;
 
+    private LocalDateTime createdAt;
 }
